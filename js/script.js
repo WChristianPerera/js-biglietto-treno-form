2993a5d44bf8ea4caaac507d8dc47c43d2f044d7
WChristianPerera <christianperera13@gmail.com>
2023-04-13 22:26:39 +0200

    Generate the Calculator

--- js/script.js
+++ js/script.js
@@ -4,45 +4,50 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. 
+/*MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+Nota:
+Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.*/
 
 
 
-//chiedere quanti chilometri percorrere
-// chiedere l'età 
-// calcolare i km in euro 
-// applicare lo sconto in base all'età
-// fixare il risultato 
 
 
 
 
+let km = document.querySelector('#km').value;
 
-
-let km = parseInt(prompt('Quanti chilometri vuoi percorrere ?'));
-
-let age = parseInt(prompt('Quanti anni hai ?'));
+let age = document.querySelector('#age');
 
 const priceForKm = 0.21;
 const discountChild = 20;
-const discountAdult = 40
+const discountAdult = 40;
 
 let basePrice = (priceForKm * km);
-let Underprice = (basePrice - (basePrice * discountChild / 100));      
-let Overprice = (basePrice - (basePrice * discountAdult / 100));      
-
-if (age >= 18 && age <= 65) {
-    document.getElementById("euros").innerHTML = basePrice.toFixed(2);
-} else if (age < 18) {
-    document.getElementById("euros").innerHTML = Underprice.toFixed(2);
-} else (age > 65); {
-    document.getElementById("euros").innerHTML = Overprice.toFixed(2);
-}
+let finalPriceU = (basePrice - (basePrice * discountChild / 100));        
+let finalPriceO = (basePrice - (basePrice * discountAdult / 100));     
 
 
+const eleBtnG = document.querySelector('#generate');
+eleBtnG.addEventListener('click', function () {
+    if (age == 'Minorenne') {
+        document.getElementById("finalquote").innerHTML = finalPriceU.toFixed(2);
+        
+    } else (age == 'Over 65'); {
+        document.getElementById("finalquote").innerHTML = finalPriceO.toFixed(2) + '€';
+    }
+    if (age == 'Maggiorenne') {
+        document.getElementById("finalquote").innerHTML = basePrice.toFixed(2);
+    }
+})
+    
 
-if (isNaN(km) || isNaN(age)) {
-    document.getElementById("euros").innerHTML = alert("Attenzione! Valori non validi! (Ricaricare la pagina)");
-}
+
+
+
 
 
 
